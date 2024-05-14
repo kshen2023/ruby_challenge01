9b21765f1884f17fc723617cf7ebfe037a8e27e5
kangShen <kshen@rrc.ca>
2024-05-14 17:43:27 -0500

    question04_and_04test

--- carl_sagan.rb
+++ carl_sagan.rb
@@ -57,32 +57,41 @@ end
 require 'net/http'
 require 'json'
 
-url = 'https://data.winnipeg.ca/resource/d3jk-hb6j.json'
-uri = URI(url)
-response = Net::HTTP.get(uri)
-tree_data = JSON.parse(response)
+url = 'https://data.winnipeg.ca/resource/d3jk-hb6j.json?$limit=306000'
+#uri = URI(url)
+#response = Net::HTTP.get(uri)
+#tree_data = JSON.parse(response)
 
-# Uncomment the following line if you want to see the structure of the data
-# pp tree_data
-
-# Define a method to count Ash trees
-def count_ash_trees(tree_data)
-  ash_count = 0
-  tree_data.each do |tree|
-    if tree['common_name']&.downcase&.include?('ash')
-      ash_count += 1
-    elsif tree['scientific_name']&.downcase&.include?('fraxinus')
-      ash_count += 1
-    elsif tree['cfs_common']&.downcase&.include?('ash')
-      ash_count += 1
-    end
-  end
-  ash_count
+def get_tree_data(url)
+  uri = URI(url)
+  response = Net::HTTP.get(uri)
+  JSON.parse(response)
 end
 
-# Count the Ash trees
-ash_trees_count = count_ash_trees(tree_data)
-puts "Number of Ash trees in the dataset: #{ash_trees_count}"
+
+def count_ash_trees(tree_data)
+  ash_tree_count = 0
+
+  tree_data.each do |tree|
+    if tree['common_name']&.downcase&.include?('ash')
+      ash_tree_count += 1
+    elsif tree['scientific_name']&.downcase&.include?('fraxinus')
+      ash_tree_count += 1
+    end
+  end
+
+  ash_tree_count
+
+end
+
+url = 'https://data.winnipeg.ca/resource/d3jk-hb6j.json?$limit=306000'
+
+#Retrieve tree data from the subset URL
+tree_data = get_tree_data(url)
+
+#Count Ash trees
+ash_count = count_ash_trees(tree_data)
+puts "Number of Ash trees in Winnipeg: #{ash_count}"
 
 
 
--- challenge01_04.rb
+++ challenge01_04.rb
@@ -1,29 +1,13 @@
 require 'net/http'
 require 'json'
 
-url = 'https://data.winnipeg.ca/resource/d3jk-hb6j.json'
+url = 'https://data.winnipeg.ca/resource/d3jk-hb6j.json?$limit=306000'
 uri = URI(url)
+
 response = Net::HTTP.get(uri)
-tree_data = JSON.parse(response)
+trees = JSON.parse(response)
 
-# Uncomment the following line if you want to see the structure of the data
-# pp tree_data
+# count all the Ash trees in the dataset
+ash_tree_count = trees.count { |tree| tree['common_name'] && tree['common_name'].downcase.include?('ash') }
 
-# Define a method to count Ash trees
-def count_ash_trees(tree_data)
-  ash_count = 0
-  tree_data.each do |tree|
-    if tree['common_name']&.downcase&.include?('ash')
-      ash_count += 1
-    elsif tree['scientific_name']&.downcase&.include?('fraxinus')
-      ash_count += 1
-    elsif tree['cfs_common']&.downcase&.include?('ash')
-      ash_count += 1
-    end
-  end
-  ash_count
-end
-
-# Count the Ash trees
-ash_trees_count = count_ash_trees(tree_data)
-puts "Number of Ash trees in the dataset: #{ash_trees_count}"
+puts "Ash trees count: #{ash_tree_count}"
